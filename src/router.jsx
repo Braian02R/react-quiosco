a5a507a118022617ef855ed1c84fa598059a664f
Braian <bf02rojas.edu@gmail.com>
2025-03-01 10:35:21 -0300

    primer commit

---
+++ src/router.jsx
@@ -0,0 +1,40 @@
+import { createBrowserRouter } from 'react-router-dom'
+import { Navigate } from 'react-router-dom';
+import Layout from './layouts/Layout'
+import AuthLayout from './layouts/AuthLayout'
+import Inicio from './views/Inicio'
+import Login from './views/Login'
+import Registro from './views/Registro'
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <Inicio />
+            }
+        ]
+    },
+    {
+        path: '/auth',
+        element: <AuthLayout />,
+        children: [
+            { 
+                index: true, 
+                element: <Navigate to="/auth/registro" /> 
+            },
+            {
+                path: '/auth/login',
+                element: <Login />
+            },
+            {
+                path: '/auth/registro',
+                element: <Registro />
+            }
+        ]
+    }
+])
+
+export default router
