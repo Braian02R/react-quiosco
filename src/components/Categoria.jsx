290fe24615deb5087a323e7036319a07739208e1
Braian <bf02rojas.edu@gmail.com>
2025-03-01 18:00:12 -0300

    creacion de context para manejar estado de la app

--- src/components/Categoria.jsx
+++ src/components/Categoria.jsx
@@ -1,19 +1,27 @@
+import useQuiosco from "../hooks/useQuiosco"
+
 export default function Categoria({categoria}) {
 
     //console.log(categoria)
-
+    const { handleClickCategoria, categoriaActual } = useQuiosco()
     const {icono, id, nombre} = categoria
 
     return (
-        <div className="flex items-center gap-4 border w-full p-3
-        hover:bg-amber-400 cursor-pointer">
+        <div className={`${categoriaActual.id === id ? "bg-amber-400" : "bg-white"} flex items-center gap-4 border w-full p-3
+        hover:bg-amber-400 cursor-pointer`}>
             <img 
                 src={`/img/icono_${icono}.svg`} 
                 alt="Imagen Icono"
                 className="w-12"
             />
 
-            <p className="text-lg font-bold cursor-pointer truncate">{nombre}</p>
+            <button 
+                className="text-lg font-bold cursor-pointer truncate"
+                type="button"
+                onClick={ () => handleClickCategoria(id)} //arrow funcion para que no traiga todos los id de una pasada
+            >
+                {nombre}
+            </button>
         </div>
         
     )
