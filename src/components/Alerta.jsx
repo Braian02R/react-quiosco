e9fbb7954381bb4dcf20164a77e0b36e2aa148b6
Braian <bf02rojas.edu@gmail.com>
2025-03-08 20:22:27 -0300

    actualizacion del componente 'Alerta'  para mostrar validaciones

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,7 +1,17 @@
-export default function Alerta({children}) {
-  return (
-    <div className="text-center my-2 bg-red-600 text-white font-bold p-3 uppercase">
-        {children}
-    </div>
-  )
-}
+import React from 'react';
+
+
+export default function Alerta({ children }) {
+    const childrenArray = React.Children.toArray(children);
+    
+    return (
+      <div className="text-center my-2 bg-red-600 text-white font-bold p-3 uppercase">
+        {childrenArray.map((child, i) => (
+          <React.Fragment key={i}>
+            {child}
+            {index < childrenArray.length - 1 && <br />}
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  }
